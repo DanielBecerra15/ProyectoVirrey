24fcc98564b5dc7d362ad9afe062f585d8344031
Daniel <dbecerrapa@unbosque.edu.co>
2022-10-21 06:43:35 -0500

    Reorganizacion del codigo

--- Proyecto_v3.R
+++ Proyecto_v3.R
@@ -7,57 +7,24 @@ library(ggplot2)
 library(magrittr)
 
 
-# ECTURA DE BASE DE DATOS -------------------------------------------------
+# LECTURA DE BASE DE DATOS -------------------------------------------------
 
-base_mayo <- read_excel("C:/Users/danie/Downloads/BAA.xlsx", 
+base_mayo <- read_excel("C:/Users/danie/Desktop/Estadistica/ProgramaVirrey/VIRREY/data/BAA.xlsx",
                         sheet = "MAYO")
 
-base_junio <- read_excel("C:/Users/danie/Downloads/BAA.xlsx", 
+base_junio <- read_excel("C:/Users/danie/Desktop/Estadistica/ProgramaVirrey/VIRREY/data/BAA.xlsx", 
                          sheet = "JUNIO")
 
-base_julio <- read_excel("C:/Users/danie/Downloads/BAA.xlsx", 
+base_julio <- read_excel("C:/Users/danie/Desktop/Estadistica/ProgramaVirrey/VIRREY/data/BAA.xlsx", 
                          sheet = "JULIO")
 
-base_agosto <- read_excel("C:/Users/danie/Downloads/BAA.xlsx", 
+base_agosto <- read_excel("C:/Users/danie/Desktop/Estadistica/ProgramaVirrey/VIRREY/data/BAA.xlsx", 
                           sheet = "AGOSTO")
 
-base_septiembre <- read_excel("C:/Users/danie/Downloads/BAA.xlsx", 
+base_septiembre <- read_excel("C:/Users/danie/Desktop/Estadistica/ProgramaVirrey/VIRREY/data/BAA.xlsx", 
                               sheet = "SEPTIEMBRE")
 base<-list(base_mayo,base_junio,base_julio,base_agosto,base_septiembre)
 
-# ENTRADAS ----------------------------------------------------------------
-
-mes<-"agosto"
-cancelacion<-"NO"
-especialidad<-"CIRUGIA GENERAL"
-especialista<-"ESCOBAR DIAZ ALEJANDRO"
-procedimiento<-"HERNIORRAFIA UMBILICAL VIA ABIERTA"
-anestesiologo<-"TORRES MARTINEZ ELIANA LISEL"
-anestesia<-"REGIONAL"
-
-table(base_agosto$`ESPECIALIDAD TRATANTE`[base_agosto$`ESPECIALIDAD TRATANTE`=="CIRUGIA GENERAL"])
-# FILTROS -----------------------------------------------------------------
-
-m = switch (mes,                #Asignar valor de posicion al mes
-            "mayo" = 1,
-            "junio" = 2,
-            "julio" = 3,
-            "agosto" = 4,
-            "septiembre" = 5
-)
-#base[m]
-basem<-as.data.frame(base[m])  #Define una tabla de datos por el mes seleccionado
-
-# PURGA DE BASE DE DATOS --------------------------------------------------
-
-basem$COMPLICACIONES[is.na(basem$COMPLICACIONES)] <- "NO"
-basem$MORTALIDAD[is.na(basem$MORTALIDAD)] <- "NO"
-basem$PATOLOGIA[is.na(basem$PATOLOGIA)] <- "NO"
-basem$PROFILAXIS.ANTIBIOTICA[is.na(basem$PROFILAXIS.ANTIBIOTICA)] <- "NO"
-basem$TIPO.DE.ANESTESIA[basem$TIPO.DE.ANESTESIA=="REGIONAL : SEDACION"] <- "REGIONAL + SEDACION"
-basem$FECHA.DE.CIRUGIA<-as.character.Date(basem$FECHA.DE.CIRUGIA)
-names(basem)[25]<-"CAUSA.DE.CANCELACION"
-variable.names(basem)
 
 # FUNCIONES ---------------------------------------------------------------
 
@@ -74,6 +41,39 @@ resumen<-function(x){
   hist_general(x)
   tablas(x)
 }
+
+# ENTRADAS ----------------------------------------------------------------
+
+mes<-"agosto"
+cancelacion<-"NO"
+especialidad<-"CIRUGIA GENERAL"
+especialista<-"ESCOBAR DIAZ ALEJANDRO"
+procedimiento<-"HERNIORRAFIA UMBILICAL VIA ABIERTA"
+anestesiologo<-"TORRES MARTINEZ ELIANA LISEL"
+anestesia<-"REGIONAL"
+
+# FILTRO POR MES-----------------------------------------------------------
+
+m = switch (mes,                #Asignar valor de posicion al mes
+            "mayo" = 1,
+            "junio" = 2,
+            "julio" = 3,
+            "agosto" = 4,
+            "septiembre" = 5
+)
+
+basem<-as.data.frame(base[m])  #Define una tabla de datos por el mes seleccionado
+
+# PURGA DE BASE DE DATOS --------------------------------------------------
+
+basem$COMPLICACIONES[is.na(basem$COMPLICACIONES)] <- "NO"
+basem$MORTALIDAD[is.na(basem$MORTALIDAD)] <- "NO"
+basem$PATOLOGIA[is.na(basem$PATOLOGIA)] <- "NO"
+basem$PROFILAXIS.ANTIBIOTICA[is.na(basem$PROFILAXIS.ANTIBIOTICA)] <- "NO"
+basem$TIPO.DE.ANESTESIA[basem$TIPO.DE.ANESTESIA=="REGIONAL : SEDACION"] <- "REGIONAL + SEDACION"
+basem$FECHA.DE.CIRUGIA<-as.character.Date(basem$FECHA.DE.CIRUGIA)
+names(basem)[25]<-"CAUSA.DE.CANCELACION"
+variable.names(basem)
 
 # ELIMINACION DE COLUMNAS -------------------------------------------------
 
@@ -101,17 +101,27 @@ switch (cancelacion,           #Filtro de cancelación
 )
 resumen(basec)
 
+                  #Filtro de especialidad
+
 basee <- basec %>% subset(basec$ESPECIALIDAD.TRATANTE==especialidad) 
 basee <- basee %>% select(-ESPECIALIDAD.TRATANTE) %>% resumen
+
+                  #filtro de especialista
 
 basesp <- basee %>% subset(basee$CIRUJANO.TRATANTE==especialista) 
 basesp <- basesp %>% select(-CIRUJANO.TRATANTE) %>% resumen
 
+                  #Filtro de procedimiento
+
 basep <- basesp %>% subset(basesp$PROCEDIMIENTO.1==procedimiento) 
 basep <- basep %>% select(-PROCEDIMIENTO.1)%>% resumen
 
+                  #Filtro de anestesiólogo
+
 basea <- basep %>% subset(basep$ANESTESIOLOGO==anestesiologo)  
 basea <- basea %>% select(-ANESTESIOLOGO)  %>% resumen
+
+                  #Filtro de tipo de anestesia
 
 basean <- basea %>% subset(basea$TIPO.DE.ANESTESIA==anestesia)  
 basean <- basean %>% select(-TIPO.DE.ANESTESIA) %>% resumen
